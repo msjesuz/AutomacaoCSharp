46aaabc16891e7eb64af9660b87531f40570b54b
Marcelo Silva Jesuino <msj_jesuz@hotmail.com>
2021-03-09 03:20:17 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutomacaoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
